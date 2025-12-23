a0ea8277d2b0490f37ef8ffe9afe023b67c49ca1
sherif-abdelsalam <sherif.abdelsalam.111@gmail.com>
2025-12-23 18:58:15 +0200

    added list db

--- db_handler/list_db.sh
+++ db_handler/list_db.sh
@@ -1,2 +1,23 @@
-#! /bin/bash
-#
+#!/bin/bash
+
+DBS_META="databases.meta"
+
+if [[ ! -f "$DBS_META" ]]; then
+    echo "NO file with that name exists!"
+    exit 0
+fi
+
+if [[ ! -s "$DBS_META" ]]; then
+    echo "No databases found."
+    exit 0
+fi
+
+awk -F':' '
+BEGIN {
+    print "No\t\tDatabase\t\tCreated At\t\tOwner"
+    print "--------------------------------------------------------------------"
+}
+{
+    print NR "\t\t" $1 "\t\t" $2 "\t\t" $3
+}
+' "$DBS_META"
