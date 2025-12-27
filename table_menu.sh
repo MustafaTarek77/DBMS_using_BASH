2be1ceca0aa91b16fd13dd906557cb339e19802c
sherif-abdelsalam <sherif.abdelsalam.111@gmail.com>
2025-12-27 12:05:23 +0200

    modified table_menu and db_meta

--- table_menu.sh
+++ table_menu.sh
@@ -9,17 +9,21 @@ select choice in \
     "Create Table" \
     "List Tables" \
     "Drop Table" \
-    "Insert Row" \
-    "Select Data" \
+    "Insert Into Table" \
+    "Select From Table" \
+    "Delete From Table" \
+    "Update Table" \
     "Back to Main Menu"
 do
     case $REPLY in
         1) ./table_handler/create_table.sh "$DB_NAME" ;;
         2) ./table_handler/list_tables.sh "$DB_NAME" ;;
         3) ./table_handler/drop_table.sh "$DB_NAME" ;;
-        4) ./table_handler/insert_row.sh "$DB_NAME" ;;
-        5) ./table_handler/select_data.sh "$DB_NAME" ;;
-        6) break ;;
+        4) ./table_handler/insert_into_table.sh "$DB_NAME" ;;
+        5) ./table_handler/select_from_table.sh "$DB_NAME" ;;
+        6) ./table_handler/delete_from_table.sh "$DB_NAME" ;;
+        7) ./table_handler/update_table.sh "$DB_NAME" ;;
+        8) break ;;
         *) echo "Invalid option" ;;
     esac
 done
