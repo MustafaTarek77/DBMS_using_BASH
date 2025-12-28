2d57895b70e06314ffa92a718434a87a5f896da0
sherif-abdelsalam <sherif.abdelsalam.111@gmail.com>
2025-12-28 17:36:18 +0200

    added reusable validations

---
+++ utils/validations.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+is_valid_name() {
+    if [[ ! "$1" =~ ^[a-zA-Z_][a-zA-Z0-9_]*$ ]]; then
+        echo "ERROR: Invalid Name"
+        exit
+    fi
+}
+is_non_empty() {
+    if [[ -z "$1" ]]; then
+        echo "ERROR: Name is required"
+        exit
+    fi
+}
+is_db_does_not_exist() {
+    if [[ -d "db_storage/$1" ]]; then
+        echo "ERROR: Database '$1' already exists"
+        exit
+    fi
+}
+
+is_db_exists() {
+    if [[ ! -d "db_storage/$1" ]]; then
+        echo "ERROR: Database '$1' does not exist"
+        exit
+    fi
+}
